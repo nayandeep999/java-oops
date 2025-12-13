9938e12bb7910a5d2b563937b6cd5edff04d818b
Nayandeep <nayandeepsen@gmail.com>
2025-12-13 23:11:26 +0530

    added comments to explain static fields from parents also gets inherited to child (child has his own copy) but cant directly acces it

--- src/com/kodewala/inheritance/Payment.java
+++ src/com/kodewala/inheritance/Payment.java
@@ -1,81 +1,66 @@
 package com.kodewala.inheritance;
 
-class UPI extends Payment {
-
-	UPI() {
-		setAmount(1000);
-		setDestAccNum("003");
-		setPaymentType("CuPI");
-		processPayment();
-
+//Child classes with constructors accepting parameters
+class CreditCard extends Payment {
+	CreditCard(String paymentType, int amount, String destAccNum) {
+		super(paymentType, amount, destAccNum);
 	}
-
 }
 
 class NetBanking extends Payment {
-
-	NetBanking() {
-		setAmount(1000);
-		setDestAccNum("001");
-		setPaymentType("NetBanking");
-		processPayment();
-
+	NetBanking(String paymentType, int amount, String destAccNum) {
+		super(paymentType, amount, destAccNum);
 	}
-
 }
 
-class CreditCard extends Payment {
-
-	CreditCard() {
-		setAmount(1000);
-		setDestAccNum("002");
-		setPaymentType("CreditCard");
-		processPayment();
-
+class UPI extends Payment {
+	UPI(String paymentType, int amount, String destAccNum) {
+		super(paymentType, amount, destAccNum);
 	}
-
 }
 
+// Parent class
 public class Payment {
 
 	private String paymentType;
 	private int amount;
 	private String destAccNum;
 
-	public String getPaymentType() {
-		return paymentType;
+	// Parameterized constructor
+	public Payment(String paymentType, int amount, String destAccNum) {
+		this.paymentType = paymentType;
+		this.amount = amount;
+		this.destAccNum = destAccNum;
+		processPayment();
 	}
 
-	public void setPaymentType(String paymentType) {
-		this.paymentType = paymentType;
+	public String getPaymentType() {
+		return paymentType;
 	}
 
 	public int getAmount() {
 		return amount;
 	}
 
-	public void setAmount(int amount) {
-		this.amount = amount;
-	}
-
 	public String getDestAccNum() {
 		return destAccNum;
 	}
 
-	public void setDestAccNum(String destAccNum) {
-		this.destAccNum = destAccNum;
-	}
-
 	void processPayment() {
-		System.out.println("Payment type is: " + paymentType + " Amount is: " + amount + " Destination Account num is: "
-				+ destAccNum);
+		System.out.println(
+				"Payment type: " + paymentType + ", Amount: " + amount + ", Destination Account: " + destAccNum);
 	}
 
 	public static void main(String[] args) {
+		CreditCard cc = new CreditCard("CreditCard", 1000, "002");
+		NetBanking nb = new NetBanking("NetBanking", 1000, "001");
+		UPI upi = new UPI("UPI", 1000, "003");
 
-		CreditCard cc = new CreditCard();
-		NetBanking nb = new NetBanking();
-		UPI upi = new UPI();
+		// Private fields of a parent class are inherited by child objects (each child
+		// object has its own copy),
+		// but they cannot be accessed directly in the child. They can only be accessed
+		// or modified
+		// via the parent’s public/protected methods or constructors.
+
 	}
-
 }
