2540f082a2354afd424b20b7c364cbe8be4323a4
Nayandeep <nayandeepsen@gmail.com>
2026-01-08 15:58:36 +0530

    updated code

--- src/com/kodewala/protectedV1/Child.java
+++ src/com/kodewala/protectedV1/Child.java
@@ -2,4 +2,19 @@ package com.kodewala.protectedV1;
 
 public class Child extends Parent {
 
+	public Child() {
+		this.protectedMethod();
+		System.out.println("protectedMethod() called from Child constructor");
+	}
+
+	public static void main(String[] args) {
+		Parent p = new Child();
+		p.protectedMethod();
+
+		Child c = new Child();
+		c.protectedMethod(); // Since we are inside child scope we can access this method using child
+		// reference as well
+
+	}
+
 }
--- src/com/kodewala/protectedV2/Driver.java
+++ src/com/kodewala/protectedV2/Driver.java
@@ -1,6 +1,6 @@
 package com.kodewala.protectedV2;
 
-public class Driver {
+public class Driver implements Cloneable {
 	public static void main(String[] args) {
 		GrandChild gc = new GrandChild();
 		// gc.protectedMethod(); // The method protectedMethod() from the type Parent is
@@ -8,12 +8,27 @@ public class Driver {
 		// Protected = inheritance-only access OR same package
 		// since protectedMethod() is present inside protectedV1 package
 		// And you are trying to call this inside protectedV2 package
-		// It does not works
+		// & Its out of the Grandchild scope -> It will not work
 		// just overwrite protectedMethod() and inside body call super.protectedMethod()
 		// Done now you can call
 
 		gc.protectedMethod();
 
 		// Protected is like default PLUS subclass access across packages
+
+		Driver d1 = new Driver();
+		try {
+			d1.clone();
+			// You can access clone and call it because this class call clone on its
+			// own object not other class object
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+
+		String s1 = "hello";
+		// String s2 = s1.clone(); // you can't call clone on String object even though
+		// String class implicitly extends Object class.
+		// Only Driver can call clone() on its own object
+		// Not on any other object
 	}
 }
