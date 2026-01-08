585a94af0fe2778a166967d73999f212c08efbcc
Nayandeep <nayandeepsen@gmail.com>
2026-01-08 14:37:22 +0530

    accessing protected via parent reference

--- src/com/kodewala/protectedV1/Driver.java
+++ src/com/kodewala/protectedV1/Driver.java
@@ -5,5 +5,8 @@ public class Driver {
 	public static void main(String[] args) {
 		Child c = new Child();
 		c.protectedMethod();
+
+		Parent p = new Parent();
+		p.protectedMethod(); // we can access protected method via parent object as well
 	}
 }
